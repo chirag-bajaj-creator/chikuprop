0e2df51fcaa24622af4d9d0d9103f13907ed202f
chirag-bajaj-creator <chiragbajaj2203@gmail.com>
2026-03-28 13:44:14 +0530

    fixed local error

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const TOKEN_KEY = "chikuprop_token";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.API_URL || "http://localhost:5000/api",
 });
 
 // Attach Bearer token to every request
